cf00f49b6c97fdd1a63a8e81d714dde59d6dd1bc
weekendfish <yusen356@gmail.com>
2025-02-10 22:50:00 +0000

    fix ci

--- rename.sh
+++ rename.sh
@@ -1,7 +1,6 @@
 # find ./tutorial -type f -name "*.md" ! -name "*_en*" -exec bash -c 'mv "$1" "${1%.md}.zh.md"' bash {} \; && \
 # find ./tutorial -type f -name "*_en*.md" -exec bash -c 'mv "$1" "${1//_en/}"' bash {} \; && \
 mkdir -p docs/setup && \
-cp bpftime/README.md docs/bpftime/index.md && \
 mkdir -p docs/tutorials && \
 cp -rf tutorial/src/* docs/tutorials/ && \
 mv tutorial/src/SUMMARY.zh.md docs/tutorials/index.zh.md && \
