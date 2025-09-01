e26c184e266d2648fdad6c143d4fe93e918a7bf0
yunwei37 <yunwei356@gmail.com>
2025-09-01 11:16:42 +0000

    1548b8ace

--- tutorials/12-profile/build.rs
+++ tutorials/12-profile/build.rs
@@ -12,14 +12,31 @@ fn main() {
         PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR must be set in build script"));
     out.push("profile.skel.rs");
 
-    let arch = env::var("CARGO_CFG_TARGET_ARCH")
-        .expect("CARGO_CFG_TARGET_ARCH must be set in build script");
+    let _arch = env::var("CARGO_CFG_TARGET_ARCH")
+        .unwrap_or_else(|_| "x86_64".to_string());
+    
+    // Map Rust target arch to kernel arch naming
+    let kernel_arch = match _arch.as_str() {
+        "x86_64" => "x86",
+        "aarch64" => "arm64",
+        "arm" => "arm",
+        "powerpc64" => "powerpc",
+        "mips" | "mips64" => "mips",
+        "riscv64" => "riscv",
+        "loongarch64" => "loongarch",
+        _ => "x86", // default to x86
+    };
+
+    // Path to the vmlinux directory containing arch-specific headers
+    let vmlinux_path = format!("../third_party/vmlinux/{}", kernel_arch);
 
     SkeletonBuilder::new()
         .source(SRC)
         .clang_args([
             OsStr::new("-I"),
-            Path::new("../vmlinux").as_os_str()
+            Path::new(&vmlinux_path).as_os_str(),
+            OsStr::new("-I"),
+            Path::new(".output").as_os_str(),
         ])
         .build_and_generate(out)
         .expect("bpf compilation failed");
